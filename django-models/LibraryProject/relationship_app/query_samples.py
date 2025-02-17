bc5ffd2ce0448711ceac1bef7b01d188a871c0e6
IveeDev <iviidev899@gmail.com>
2025-02-17 06:42:05 +0100

    Create models and relationship

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,27 @@
+import os
+import django
+
+# Set up Django environment
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_models.settings")  # Change django_models to your project name
+django.setup()
+
+from relationship_app.models import Author, Library, Book, Librarian
+
+def get_books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    return author.book_set.all()
+    
+   
+
+def get_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
+    
+
+
+# Retrieve the librarian for a library
+
+def get_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.librarian
+    
