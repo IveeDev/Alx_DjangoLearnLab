e083c6aa7782c658974cfe77a557fe6da6a3d724
IveeDev <iviidev899@gmail.com>
2025-02-22 12:02:58 +0100

    Add user authentication feature in the relationship_app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .views import LibraryDetailView
 from .views import list_books
-
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import SignupView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
-    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail')
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', SignupView.as_view(), name='register'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 from .models import Library
 from .models import Book
 # Create your views here.
@@ -15,3 +19,16 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+    
+    
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "register.html"
+    
+
+# class CustomLoginView(LoginView):
+#     template_name = 'login.html'
+    
+# class CustomLogoutView(LogoutView):
+#     template_name = 'logout.html'
