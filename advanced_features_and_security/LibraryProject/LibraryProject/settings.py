eb3228ea2ea74e1d72caf447b52eb9f98eebe09a
IveeDev <iviidev899@gmail.com>
2025-03-01 12:45:42 +0100

    Modify

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -128,4 +128,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "login"
 
-AUTH_USER_MODEL = "core.CustomUser"
+AUTH_USER_MODEL = "bookshelf.CustomUser"
