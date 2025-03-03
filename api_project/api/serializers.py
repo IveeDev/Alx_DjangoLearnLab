5a3c78f60b74b129b4c5d488d76a41038645ad31
IveeDev <iviidev899@gmail.com>
2025-03-03 15:17:04 +0100

    Add enpoint for retrieving all books

---
+++ api_project/api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+    model = Book
+    fields = ["id", "title", "author"]
+    
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+# from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from .serializers import BookSerializer
+from .models import Book
 
 # Create your views here.
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
