cefbd44aa4347913cc72cec55bc50b2940f06132
IveeDev <iviidev899@gmail.com>
2025-03-09 16:45:37 +0100

    Implement permission in the BookVieSet

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import BookViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 router = DefaultRouter()
@@ -8,6 +9,7 @@ router.register(r'books', BookViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api/token/', obtain_auth_token, name='api_token_auth'),
 ]
 
 # urlpatterns = [
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, ListModelMixin
 from .serializers import BookSerializer
 from .models import Book
+from .permissions import IsOwnerOrReadOnly
 
 # Create your views here.
 # class BookList(generics.ListAPIView):
@@ -12,3 +15,4 @@ from .models import Book
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
