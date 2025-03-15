63e39adee9f81fedb711e504fb9736bcaf45680b
IveeDev <iviidev899@gmail.com>
2025-03-15 07:41:30 +0100

    Modify

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
 from .serializers import BookSerializer, AuthorSerializer
 from .models import Book, Author
 
@@ -14,12 +14,12 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     filterset_fields = ['publication_year']
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
