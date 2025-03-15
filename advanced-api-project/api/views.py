fa35a522695de62eeac7396f82f9b23bc50e2982
IveeDev <iviidev899@gmail.com>
2025-03-15 07:52:10 +0100

    Modify

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.permissions import IsAuthenticated
 from .serializers import BookSerializer, AuthorSerializer
 from .models import Book, Author
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # Create your views here.
 class BookCreateView(generics.CreateAPIView):
