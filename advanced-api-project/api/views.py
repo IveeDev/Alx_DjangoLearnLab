539407fe19f62153573f0429fb3e8a748abf2d7d
IveeDev <iviidev899@gmail.com>
2025-03-15 07:47:06 +0100

    Modify

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import (IsAuthenticatedOrReadOnly, IsAuthenticated)
 from .serializers import BookSerializer, AuthorSerializer
 from .models import Book, Author
 
