91f432de9fd7df949adb5595994ae4a981bdbcd0
IveeDev <iviidev899@gmail.com>
2025-03-29 22:38:40 +0100

    Modify

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -8,20 +8,25 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=150)  # serializers.CharField()
+    bio = serializers.CharField(allow_blank=True, required=False)
+    profile_picture = serializers.ImageField(upload_to='profile_pics/images', blank=True, null=True) 
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
         
 
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+class UserRegisterSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=150)  # serializers.CharField()
+    email = serializers.CharField(max_length=255)  # serializers.CharField()
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
         
     def create(self, validated_data):
         user = get_user_model().objects.create_user(**validated_data)
-        # Token.objects.create(user=user)
+        Token.objects.create(user=user)
         return user
     
     
