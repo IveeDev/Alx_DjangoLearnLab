3e49a9812def5cc5d174d7f18437c6e6def3eab2
IveeDev <iviidev899@gmail.com>
2025-03-31 10:01:54 +0100

    modify import statement

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .serializers import PostSerializer, CommentSerializer
@@ -7,7 +7,7 @@ from .models import Post, Comment
 
 # Create your views here.
 
-class PostViewSet(ModelViewSet):
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
@@ -18,7 +18,7 @@ class PostViewSet(ModelViewSet):
         serializer.save(author=self.request.user)
 
 
-class CommentViewSet(ModelViewSet):
+class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all().order_by('created_at')
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
